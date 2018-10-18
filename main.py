8a80b911579650981ea276eb41798fc7dd2d0580
Andrei Rj <arjevskii@live.com>
2018-10-18 17:50:12 -0400

    Update main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import wolframalpha
+import os
+
+#clears the terminal/window screen
+os.system('clear')
+
+#Input the api key here, should. Example -> ("G8YTHF-Y2967PT5H1")
+client= wolframalpha.Client("T8YRHK-W2952PT7Y6") #Input the api key here, should. Example.      wolframalpha.Client("G8YTHF-Y2967PT5H1")
+
+'''remove comment if the questions is pre-written
+input_string = "What is the weather in toronto today?"
+'''
+
+#or ask in python input the question
+input_string=input("Ask me a question:")
+
+#outputs the reults
+output_string = client.query(input_string)
+try:
+    print (next(output_string.results).text)
+
+except StopIteration:
+    print ("No results")
